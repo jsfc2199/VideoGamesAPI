3ff29a1dfb5eefb6bfa4efdb27b74cfe81e81fcd
Juan Sebastian <jsfc2199@gmail.com>
2022-05-01 18:20:53 -0500

    Project started: Video games API

---
+++ src/main/java/com/example/videogameswebapp/VideoGamesWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.videogameswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGamesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGamesWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/videogameswebapp/VideoGamesWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videogameswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGamesWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
